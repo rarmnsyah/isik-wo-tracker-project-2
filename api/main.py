e943288659edec329fc24f651fecd388e714e090
Risky Armansyah <rarmnsyah787@gmail.com>
2025-09-19 09:43:26 +0700

    build: delete router for main endpoint

--- api/main.py
+++ api/main.py
@@ -2,18 +2,16 @@ from fastapi import FastAPI, APIRouter
 from .routers import workouts, teams, webhook
 
 app = FastAPI(title="isik-wo-project API")
-router = APIRouter(prefix='/api/py')
 
-@router.get("")
+@app.get("")
 async def read_category_by_query():
     role_to_return ="mantap"
     return role_to_return
 
-@router.get("/health")
+@app.get("/health")
 def health():
     return {"ok": True}
 
-app.include_router(router)
 app.include_router(webhook.router)
 app.include_router(workouts.router)
 app.include_router(teams.router)
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,8 @@
 export default async function MyNextFastAPIApp() {
-  const role = await fetchEngineerRole();
 
   return (
     <>
-      <div>{`The main skill of a ${role}`}</div>
+      <div>{`Hello`}</div>
     </>
   );
-}
-
-async function fetchEngineerRole() {
- 
-  const baseUrl = "http://localhost:3000";
-
-  try { 
-    const response = await fetch(
-      `${baseUrl}/api/py`
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const role = await response.json();
-    return role;
-  } catch (error) {
-
-    console.error("Error fetching engineer role:", error);
-    return null;
-  }
 }

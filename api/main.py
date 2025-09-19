55d488c7fccc01277eed63b51ca7e45fccd0ce6f
Risky Armansyah <rarmnsyah787@gmail.com>
2025-09-19 09:48:02 +0700

    fix: return main endpoint router

--- api/main.py
+++ api/main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI, APIRouter
 from .routers import workouts, teams, webhook
 
 app = FastAPI(title="isik-wo-project API")
+router = APIRouter(prefix='/api/py')
 
-@app.get("")
+@router.get("")
 async def read_category_by_query():
     role_to_return ="mantap"
     return role_to_return
 
-@app.get("/health")
+@router.get("/health")
 def health():
     return {"ok": True}
 
+app.include_router(router)
 app.include_router(webhook.router)
 app.include_router(workouts.router)
 app.include_router(teams.router)
